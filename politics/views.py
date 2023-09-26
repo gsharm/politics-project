96188fa1d32bbb20387e36f8fc276905afb718eb
gsharm <gsharm@gmail.com>
2023-09-26 18:44:46 +0100

    added css

--- politics/views.py
+++ politics/views.py
@@ -6,11 +6,19 @@ from django.http import HttpResponse
 
 def serve_markdown_index(request):
     mps = list_mps()
-
-    # Convert list of MPs to HTML links
     html_links = [f'<a href="/mps/{mp}/">{mp.replace("-", " ").title()}</a><br>' for mp in mps]
-    html_content = "<h2>List of MPs:</h2>" + "\n".join(html_links)
-
+    
+    html_content = f'''
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/static/css/styles.css">
+    </head>
+    <body>
+        <h2>List of MPs:</h2>
+        {"\n".join(html_links)}
+    </body>
+    </html>
+    '''
     return HttpResponse(html_content)
 
 def serve_markdown_mp(request, mp_name=None):
@@ -30,9 +38,20 @@ def serve_markdown_mp(request, mp_name=None):
             md_content = file.read()
 
     # Convert to HTML
+    # Wrap the HTML content to include the CSS
     html_content = markdown.markdown(md_content)
+    full_html_content = f'''
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/static/css/styles.css">
+    </head>
+    <body>
+        {html_content}
+    </body>
+    </html>
+    '''
+    return HttpResponse(full_html_content)
 
-    return HttpResponse(html_content)
 
 def list_mps():
     """List all the markdown files in the mps/ directory."""
